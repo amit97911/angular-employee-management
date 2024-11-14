1468b4fe0fa7ee4c05fc24da6a2883e8cddf7b8b
Amit Patel <amit97911@gmail.com>
2024-11-14 12:54:47 +0530

    created routing

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
+
 export class LayoutComponent {
 
 }
