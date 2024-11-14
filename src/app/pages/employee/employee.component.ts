a7a7492253a6d9ad1dc55d7858b66c5aa490d7e0
Amit Patel <amit97911@gmail.com>
2024-11-14 15:08:31 +0530

    Added Parent Department

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MasterService } from '../../service/master.service';
+import { IApiResponse, IParentDept } from '../../model/interface/master';
 
 @Component({
   selector: 'app-employee',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  isformVisible = signal<boolean>(false);
+  masterService = inject(MasterService);
+  parentDeptList = signal<IParentDept[]>([])
 
+  ngOnInit(): void {
+    this.getParentDept();
+  }
+
+  getParentDept() {
+    this.masterService.getAllParentDept().subscribe((res: IApiResponse) => {
+      this.parentDeptList.set(res.data);
+    });
+  }
 }
