34d8a309b6d30ba0e5996e9956fc6ad4e33f4c23
Amit Patel <amit97911@gmail.com>
2024-11-14 11:55:02 +0530

    static login

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Credits
 [Learning Partner](https://www.youtube.com/watch?v=WVw4ZXoWfaM)
-
+[Athul369 | Simple Login Form | CodePen] (https://codepen.io/Athul369)
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router) { }
+  loginObj: any = {
+    Username: '',
+    Password: ''
+  }
 
+
+  onLogin() {
+    if (this.loginObj.Username == "admin" && this.loginObj.Password == "admin") {
+      this.router.navigateByUrl('dashboard');
+    } else {
+      alert("Wrong Creds");
+    }
+  }
 }
